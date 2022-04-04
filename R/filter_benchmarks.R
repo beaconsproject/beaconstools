159d6877ec5dfadc8b8dbce4a5d10006dfc8b3e8
Marc <medwards219@gmail.com>
2022-04-04 11:45:02 -0700

    #25 - completed grouping functions. Will close when we have a ranking function that can extract the best benchmark/network in each group: #29

--- R/filter_benchmarks.R
+++ R/filter_benchmarks.R
@@ -1,6 +1,3 @@
-benchmarks_sf <- st_read("C:/Users/MAEDW7/Dropbox (BEACONs)/RENR491 Capstone 2022/gisdata/benchmarks/pb_i80_t100_zone1.shp")
-grid_size <- 10*1000
-
 ### group_benchmarks_using_grid ###
 #
 #' Group benchmarks based on overlap. Groups are assigned based on polygon centroid membership within 
@@ -33,9 +30,14 @@ group_benchmarks_using_grid <- function(benchmarks_sf, grid_size){
   grid_sf <- sf::st_make_grid(points_sf, cellsize = c(grid_size, grid_size), what = 'polygons') %>% 
     sf::st_sf(geometry = ., data.frame('grid_id' = 1:length(.)))
   
+  # remove grid_id if it already exists
+  if("grid_id" %in% colnames(points_sf)){
+    points_sf <- points_sf %>%
+      dplyr::select(-grid_id)
+  }
+  
   # spatial join fishnet grid id to benchmarks
   points_sf <- points_sf %>%
-    dplyr::select(-grid_id) %>%
     sf::st_join(grid_sf)
   
   # convert grid ids to ordered vector starting at 1
@@ -45,4 +47,71 @@ group_benchmarks_using_grid <- function(benchmarks_sf, grid_size){
     dplyr::left_join(mappings, by = "grid_id")
   
   return(points_sf$new_val)
+}
+
+### group_networks_using_grid ###
+#
+#' Group networks based on overlap of their component benchmarks. Individual benchmarks are first grouped using 
+#' [group_benchmarks_using_grid()]. Networks are then grouped based on the combination of their component
+#' benchmarks. Two networks with component benchmarks in matching groups will be grouped together.
+#' 
+#' e.g. for two networks PA1__PA2 and PB3__PB4, if PB1 and PB3 fall in the same gride cell, and PB2 and PB4 fall
+#' in the same grid cell, networks will receive the same grid id.
+#'
+#' @param network_list Vector of network names to group. Must be in the format x1__x2__x3 where x1, x2 and x3 are 
+#' values in the \code{benchmarks_sf} \code{benchmark_id} column. Any number of x can be used (e.g. x1__x2, 
+#' x1__x2__x3__x4 etc.).
+#' @param benchmarks_sf sf object of benchmarks or reserve polygons.
+#' @param benchmark_id unique identifier column in \code{benchmarks_sf}.
+#' @param grid_size Numeric size of grid cells to create in units matching the crs of \code{benchmarks_sf}.
+#'
+#' @return Vector of group ids matching \code{network_list}.
+#'
+#' @importFrom magrittr %>%
+#' @importFrom rlang .data
+#' @export
+#'
+#' @examples
+#' benchmarks <- catchments_to_benchmarks(
+#'   benchmark_table_sample, 
+#'   catchments_sample, 
+#'   colnames(benchmark_table_sample))
+#' network_names <- gen_network_names(benchmarks$network, 2)
+#' networks_sf <- benchmarks_to_networks(benchmarks, network_names)
+#' 
+#' group_networks_using_grid(networks_sf$network, benchmarks, "network", 10000)
+
+group_networks_using_grid <- function(network_list, benchmarks_sf, benchmark_id, grid_size){
+  
+  # get all benchmark names, check they are all in benchmarks_sf, subset benchmarks_sf
+  benchmark_names <- unique(unlist(sep_network_names(network_list)))
+  
+  if(all(benchmark_names %in% benchmarks_sf[[benchmark_id]])){
+    
+    benchmarks_sf <- benchmarks_sf %>%
+      dplyr::filter(.data[[benchmark_id]] %in% benchmark_names)
+  } else{
+    stop(paste0("Benchmarks missing from benchmarks_sf: ", benchmark_names[!benchmark_names %in% benchmarks_sf[[benchmark_id]]]))
+  }
+  
+  # group benchmarks
+  benchmarks_sf$grid_id <- group_benchmarks_using_grid(benchmarks_sf, grid_size)
+  
+  # make tibble of network names
+  tb <- dplyr::tibble(network_list = network_list)
+  
+  # calculate group_id combinations and add to tibble
+  tb$grid_id <- sapply(network_list, function(x){
+    nets <- sep_network_names(x)
+    paste0(sort(sapply(nets, function(y){benchmarks_sf$grid_id[benchmarks_sf[[benchmark_id]] == y]})), collapse = "_")
+  })
+  
+  # make table of mappings for unique grid code combinations
+  mappings <- data.frame(grid_id = unique(tb$grid_id), new_val = 1:length(unique(tb$grid_id)))
+  
+  # join output grid ids to tb
+  tb <- tb %>%
+    dplyr::left_join(mappings, by = "grid_id")
+  
+  return(tb$new_val)
 }
